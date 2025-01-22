7322993db9ee152e8c7dd6852f952317e5b1fe32
Teodor Victor <teodorv55@gmail.com>
2025-01-22 18:41:21 +0200

    .

--- start.sh
+++ start.sh
@@ -1,23 +1,3 @@
-# Install dependencies
-apt-get update && apt-get install -y \
-    wget \
-    unzip \
-    libglib2.0-0 \
-    libnss3 \
-    libgconf-2-4 \
-    libfontconfig1 \
-    libxrender1 \
-    libxext6 \
-    libx11-xcb1 \
-    libxcb1 \
-    libxcomposite1 \
-    libxdamage1 \
-    libxi6 \
-    libxtst6 \
-    fonts-liberation \
-    xdg-utils
-
-# Clean up
-apt-get clean && rm -rf /var/lib/apt/lists/*
-#!/bin/bash
-python3 main.py
+# In start.sh
+./setup.sh
+python main.py  
